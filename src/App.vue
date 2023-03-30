7a063d9ad49238b8b4ab0cb0227f56c4183a7e4b
sean-code <john.nganga@student.moringaschool.com>
2023-03-30 18:45:17 +0300

    💄Style: Update the background color of the active link and remove the tet decoration

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,13 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 10px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background: crimson;
 }
 </style>
