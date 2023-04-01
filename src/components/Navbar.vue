9c232b7b376b0e5ed7f2f517e1db66b5effab11e
sean-code <john.nganga@student.moringaschool.com>
2023-04-01 19:56:21 +0300

    Add: Redirection, Forward and backward navigation buttons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,34 @@
 <template>
-    <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link :to='{ name: "contact" }'>Contact</router-link> |
-        <router-link :to='{ name: "jobs" }'>Jobs</router-link>
-    </nav>
-    <router-view/>
+    <div class="nav">
+        <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link :to='{ name: "contact" }'>Contact</router-link> |
+            <router-link :to='{ name: "jobs" }'>Jobs</router-link>
+        </nav>
+        <button @click="redirect">Redirect</button>
+        <button @click="goBack">Go back</button>
+        <button @click="goForward">Go forward</button>
+
+        <router-view/>
+    </div>
 </template>
 
 
 <script>
+export default{
+    methods:{
+        redirect(){
+            console.log("Redirect")
+        },
+        goBack(){
+            console.log("Go Back")
+        },
+        goForward(){
+            console.log("Go Forward")
+        }
+    }
+}
 
 </script>
 
@@ -31,4 +50,11 @@
     color: white;
     background: crimson;
     }
+    button{
+        margin: 0 10px;
+        padding: 10px;
+        border: none;
+        border-radius: 4px;
+    }
+
 </style>
